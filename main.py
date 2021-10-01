60032f30b0a9d0a9a0b427ee9002e60998ed208c
Akif Akbarov <91716299+AkifAkbarov@users.noreply.github.com>
2021-10-01 06:30:12 +0000

    Updated 1st code!

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
 
-x=int(input("Enter an integer x: "))
-y=int(input("Enter an integer y: "))
+x=int(input("Enter an number x: "))
+y=int(input("Enter an number y: "))
 print("x**y = ", x**y)
 print("log2(x) = ",math.log2(x))
-print("0078456")
+print("Student ID = 0078456")
